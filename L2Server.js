a7decb71b9225cd45535f4206bb0d3518385d39e
Memnoc <smartdroidesign@gmail.com>
2019-10-25 15:40:23 +0100

    routing and  static GET request

--- L2Server.js
+++ L2Server.js
@@ -1,24 +1,29 @@
-// Express to run server routes
-const express = require('express');
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
 
-// Star an instance of the app
+// Express to run server and routes
+const express = require('express');
+const http = require('http');
+
+// Start up an instance of app
 const app = express();
 
-// Dependencies
+/* Dependencies */
 const bodyParser = require('body-parser')
-    // Middleware
-    // Configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static('demo'));
+/* Initializing the main project folder */
+app.use(express.static('Server-side'));
 
-const port = 8000;
-// Spin up the server
+const port = 3000;
+
+// TODO-Spin up the server
+
 const server = app.listen(port, listening);
 
 // Callback to debug
@@ -26,3 +31,8 @@ function listening() {
     console.log("server running");
     console.log(`running on localhost: ${port}`);
 }
+
+// GET route
+app.get("/all", function(req, res) {
+    res.send(projectData);
+});
