0b7cc4306360e3903bfbc83bc5eef900bfa8338a
Memnoc <smartdroidesign@gmail.com>
2019-11-20 13:08:24 +0000

    first webpack build

--- Webpack/webpack.dev.js
+++ Webpack/webpack.dev.js
@@ -3,19 +3,22 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+console.log("path.join() : ", path.join());
+// outputs .
+console.log("path.resolve() : ", path.resolve());
+// outputs current directory or equalent to __dirname of the node process
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
